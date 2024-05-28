58860a057d65cd1b271b84bfe9ccbb2e1224f2fa
TinaHaser <tina.haser@mni.thm.de>
2024-05-28 16:18:40 +0200

    typeorm stuff

--- myCargonaut-server/src/app.module.ts
+++ myCargonaut-server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {UserDB} from "./database/UserDB";
+import {LocationDB} from "./database/LocationDB";
+import {ReviewDB} from "./database/ReviewDB";
+import {CarDB} from "./database/CarDB";
+import {TrailerDB} from "./database/TrailerDB";
+import {TripDB} from "./database/TripDB";
+import {VehicleDB} from "./database/VehicleDB";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './db.sqlite',
+      entities: [UserDB, CarDB, LocationDB, ReviewDB, TrailerDB, TripDB, VehicleDB],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([UserDB, CarDB, LocationDB, ReviewDB, TrailerDB, TripDB, VehicleDB]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
