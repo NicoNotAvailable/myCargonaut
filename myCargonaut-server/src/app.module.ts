8cc17e8e3a6406cd09d74c1a1a19e380833286d5
TinaHaser <tina.haser@mni.thm.de>
2024-05-28 16:19:04 +0200

    linter

--- myCargonaut-server/src/app.module.ts
+++ myCargonaut-server/src/app.module.ts
@@ -1,24 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {UserDB} from "./database/UserDB";
-import {LocationDB} from "./database/LocationDB";
-import {ReviewDB} from "./database/ReviewDB";
-import {CarDB} from "./database/CarDB";
-import {TrailerDB} from "./database/TrailerDB";
-import {TripDB} from "./database/TripDB";
-import {VehicleDB} from "./database/VehicleDB";
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { UserDB } from './database/UserDB';
+import { LocationDB } from './database/LocationDB';
+import { ReviewDB } from './database/ReviewDB';
+import { CarDB } from './database/CarDB';
+import { TrailerDB } from './database/TrailerDB';
+import { TripDB } from './database/TripDB';
+import { VehicleDB } from './database/VehicleDB';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: './db.sqlite',
-      entities: [UserDB, CarDB, LocationDB, ReviewDB, TrailerDB, TripDB, VehicleDB],
+      entities: [
+        UserDB,
+        CarDB,
+        LocationDB,
+        ReviewDB,
+        TrailerDB,
+        TripDB,
+        VehicleDB,
+      ],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([UserDB, CarDB, LocationDB, ReviewDB, TrailerDB, TripDB, VehicleDB]),
+    TypeOrmModule.forFeature([
+      UserDB,
+      CarDB,
+      LocationDB,
+      ReviewDB,
+      TrailerDB,
+      TripDB,
+      VehicleDB,
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
