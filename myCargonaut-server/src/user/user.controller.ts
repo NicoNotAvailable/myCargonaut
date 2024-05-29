9323da6f3603dc1ec1edfaa7ec828640a4f818d6
tinahsr <tinahaser@gmail.com>
2024-05-29 13:52:04 +0200

    post user finished kinda

---
+++ myCargonaut-server/src/user/user.controller.ts
@@ -0,0 +1,51 @@
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { CreateUserDTO } from './DTO/CreateUserDTO';
+import { OkDTO } from '../serverDTO/OkDTO';
+
+@ApiTags('user')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  /*
+   * uses the userService to create a new user with the given values that are needed in order to create a new user
+   * @Param body CreateUserDTO, username, firstname, lastname, password
+   * @Return OkDTO if the user was succesfully created
+   * @Error if the input values do not meet the given requirements
+   * @Error if the user could not be created
+   */
+  @ApiResponse({ type: OkDTO, description: 'creates a new user' })
+  @Post()
+  async createUser(@Body() body: CreateUserDTO) {
+    if (body.password.trim() === '' || body.password.trim().length == 0) {
+      throw new BadRequestException('Passwort darf nicht leer sein');
+    }
+    if (body.password.trim().length < 8) {
+      throw new BadRequestException(
+        'Passwort muss mindestens 8 Zeichen lang sein',
+      );
+    }
+    if (body.email.trim().length == 0 || body.email.trim() === '') {
+      throw new BadRequestException('Email darf nicht leer sein');
+    }
+    if (body.firstName.trim().length == 0 || body.firstName.trim() === '') {
+      throw new BadRequestException('Vorname darf nicht leer sein');
+    }
+    if (body.lastName.trim().length == 0 || body.lastName.trim() === '') {
+      throw new BadRequestException('Nachname darf nicht leer sein');
+    }
+    try {
+      await this.userService.createUser(
+        body.firstName,
+        body.lastName,
+        body.email,
+        body.password,
+      );
+      return new OkDTO(true, 'User was created');
+    } catch (err) {
+      throw new BadRequestException('E-Mail gibt es schon');
+    }
+  }
+}
---
+++ myCargonaut-server/src/user/user.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserDB } from '../database/UserDB';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserDB)
+    private userRepository: Repository<UserDB>,
+  ) {}
+
+  async createUser(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+  ): Promise<UserDB> {
+    const newUser: UserDB = this.userRepository.create();
+    const checkUser: UserDB = await this.userRepository.findOne({
+      where: { email: email },
+    });
+    if (checkUser != null && email === checkUser.email) {
+      throw new BadRequestException('email already exists');
+    }
+    newUser.firstName = firstName;
+    newUser.lastName = lastName;
+    newUser.email = email;
+    newUser.password = password;
+    return await this.userRepository.save(newUser);
+  }
+}
