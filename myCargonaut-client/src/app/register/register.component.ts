e6bcc6fcf36c4dcb7b49d92d51d540252469bf6a
Fabian Fimmen <fabian@ourhost.de>
2024-05-29 20:16:19 +0200

    durchstich funktioniert, Telefonnummer und Bild müssen noch gemacht werden

--- myCargonaut-client/src/app/register/register.component.ts
+++ myCargonaut-client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import {NgIf} from "@angular/common";
+import {NgbInputDatepicker} from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -11,11 +12,14 @@ import {NgIf} from "@angular/common";
     FormsModule,
     HttpClientModule,
     NgIf,
+    NgbInputDatepicker,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  message: string = ""
 
   firstName: string = "";
   lastName: string = "";
@@ -23,12 +27,19 @@ export class RegisterComponent {
   emailConfirm: string = "";
   password: string = "";
   passwordConfirm: string = "";
-  birthdate: string = "";
   phonenumber: string = "";
   agb: boolean = false;
 
+  birthDate: any ="";
+
+
+  birthDay: number = 0;
+  birthMonth: number = 0;
+  birthYear: number = 0;
+
   selectedFile: File | null = null;
   profilePicRoute: string = "";
+
 
   constructor(private http: HttpClient) {
 
@@ -79,9 +90,24 @@ export class RegisterComponent {
     }
   }
 
-  addUser(event: SubmitEvent): void {
+  showInputDate(){
+    console.log(this.birthDate.year + this.birthDate.month + this.birthDate.day )
+  }
 
-    event.preventDefault();
+  onSubmit(form: any) {
+    console.log('Form submitted:', form);
+    // Form reset
+    this.firstName = '';
+    form.resetForm();
+  }
+
+
+  addUser(form: any): void {
+
+
+    let date: Date = new Date(this.birthDate.year, this.birthDate.month -1 , this.birthDate.day);
+
+    console.log(date);
 
     const userData = {
       firstName: this.firstName,
@@ -90,18 +116,28 @@ export class RegisterComponent {
       emailConfirm: this.emailConfirm,
       password: this.password,
       passwordConfirm: this.passwordConfirm,
-      birthdate: this.birthdate,
+      birthday: date,
       phonenumber: this.phonenumber,
       agb: this.agb,
     };
 
-    this.http.post("http://localhost:4200/user", userData)
+    this.http.post("http://localhost:8000/user", userData)
       .subscribe(
         response => {
+          form.resetForm();
           console.log('User added successfully', response);
+          this.message = "Nutzer erfolgreich angelegt";
+          setTimeout(() => {
+            this.message = '';
+          }, 5000);
+
         },
         error => {
           console.error('There was an error!', error);
+          this.message = "Bitte überprüfen Sie die Eingabe";
+          setTimeout(() => {
+            this.message = '';
+          }, 5000);
         }
       );
   }
