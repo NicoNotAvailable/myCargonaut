a16be8b60b505b1e1d037a8b5e14dede5d6084f6
Fabian Fimmen <fabian@ourhost.de>
2024-05-29 12:40:53 +0200

    fetch in app.config eingebunden

--- myCargonaut-client/src/app/app.config.ts
+++ myCargonaut-client/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(withFetch()), // HttpClient mit Fetch API konfigurieren
+    provideRouter(routes)
+  ]
 };
