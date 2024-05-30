aa9eca523c7ca5c42c948887148d3fd7fe28c8fe
Nico Hofmann <nico.hofmann@mni.thm.de>
2024-05-30 13:43:02 +0200

    login might be done? it would be cery swag frrfrfrfrfrfrfrfrfrp please listen to twenty one pilots' new album clancy (out may 24th) very great album

--- myCargonaut-server/src/session/DTO/LoginDTO.ts
+++ myCargonaut-server/src/session/DTO/LoginDTO.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class LoginUserDTO {
+export class LoginDTO {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
--- myCargonaut-server/src/session/session.controller.ts
+++ myCargonaut-server/src/session/session.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  Session,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserDB } from '../database/UserDB';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Session, SessionData } from 'express-session';
+import { SessionData } from 'express-session';
 import { LoginUserDTO } from './DTO/LoginUserDTO';
 import { OkDTO } from '../serverDTO/OkDTO';
+import { LoginDTO } from './DTO/LoginDTO';
 
 @ApiTags('session')
 @Controller('session')
@@ -13,18 +20,24 @@ export class SessionController {
   constructor(
     @InjectRepository(UserDB)
     private readonly userRepo: Repository<UserDB>,
-  ) {} /*
+  ) {}
   @Post('login')
   async loginUser(
     @Session() session: SessionData,
-    @Body body: LoginUserDTO,
+    @Body() body: LoginDTO,
   ): Promise<OkDTO> {
     if (body.password === '' || body.email === '') {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Felder müssen ausgefüllt sein');
     }
     const loggedUser: LoginUserDTO = await this.userRepo.findOneBy({
       email: body.email,
       password: body.password,
     });
-  }*/
+    if (loggedUser) {
+      session.currentUser = loggedUser.id;
+    } else {
+      throw new UnauthorizedException('Passwort oder Email ist falsch');
+    }
+    return new OkDTO(true, 'User was logged in');
+  }
 }
