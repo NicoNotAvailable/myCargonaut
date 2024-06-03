8c638cbdd96b5d4f4b4e86dd5924369418eb221f
Nico Hofmann <nico.hofmann@mni.thm.de>
2024-06-03 14:27:39 +0200

    front end start

---
+++ myCargonaut-client/src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './frontpage.component.html',
+  styleUrl: './frontpage.component.css'
+})
+export class FrontpageComponent {
+
+}
