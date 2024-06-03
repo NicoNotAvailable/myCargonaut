a13227616ae349cb8a80bc3e80b2cb6d01db3e56
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-06-03 15:36:44 +0200

    Navbar 2024.06.03-2

---
+++ myCargonaut-client/src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faCar, faCircle, faCirclePlus, faHouse, faPlus, faUser} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    FaIconComponent
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  protected readonly faCar = faCar;
+  protected readonly faHouse = faHouse;
+  protected readonly faUser = faUser;
+  protected readonly faCirclePlus = faCirclePlus;
+  protected readonly faPlus = faPlus;
+  protected readonly faCircle = faCircle;
+}
