fead3b2f8520493c1cb3dfb68ad530be5d1865c4
Alexander Mutig <alexander.mutig@mni.thm.de>
2024-06-04 13:32:51 +0200

    Minor html fixes, added car overview html

---
+++ myCargonaut-client/src/app/profile/car-overview/car-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-overview.component.html',
+  styleUrl: './car-overview.component.css'
+})
+export class CarOverviewComponent {
+
+}
--- myCargonaut-client/src/app/profile/profile.component.ts
+++ myCargonaut-client/src/app/profile/profile.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserDescriptionComponent } from "./user-description/user-description.component";
 import { AddCarComponent } from "./add-car/add-car.component";
 import { AddTrailerComponent } from "./add-trailer/add-trailer.component";
+import { CarOverviewComponent } from "./car-overview/car-overview.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [UserDescriptionComponent, AddCarComponent, AddTrailerComponent],
+  imports: [UserDescriptionComponent, AddCarComponent, AddTrailerComponent, CarOverviewComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
