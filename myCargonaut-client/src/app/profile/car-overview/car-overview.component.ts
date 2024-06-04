7d51b5fffbe92658bf58fcba8f9efa57e2f756fd
Nico Hofmann <nico.hofmann@mni.thm.de>
2024-06-04 21:46:02 +0200

    some frontend fixes

--- myCargonaut-client/src/app/profile/car-overview/car-overview.component.ts
+++ myCargonaut-client/src/app/profile/car-overview/car-overview.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-car-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './car-overview.component.html',
   styleUrl: './car-overview.component.css'
 })
 export class CarOverviewComponent {
 
+  protected readonly faXmark = faXmark;
+  protected readonly faPlus = faPlus;
 }
