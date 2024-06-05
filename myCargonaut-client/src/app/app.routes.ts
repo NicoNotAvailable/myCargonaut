60b901fd486690c1a58450c02647ab13dc53e47f
Fabian Fimmen <fabian@ourhost.de>
2024-06-05 14:35:49 +0200

    register Button angepasst
    routing von frontpage
    
    frontpage text und noch kein account section

--- myCargonaut-client/src/app/app.routes.ts
+++ myCargonaut-client/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
+import {FrontpageComponent} from "./frontpage/frontpage.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', component: FrontpageComponent },
 
 ];
