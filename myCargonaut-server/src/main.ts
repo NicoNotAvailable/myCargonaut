81d2944c41475d608ce82ca4dd52b3fedab262ac
sirteddi <annalena@as-schaefer.de>
2024-06-07 12:10:49 +0200

    getuser tests, + get user

--- myCargonaut-server/src/main.ts
+++ myCargonaut-server/src/main.ts
@@ -6,43 +6,43 @@ import * as express from 'express';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 declare module 'express-session' {
-    interface SessionData {
-        currentUser?: number;
-    }
+  interface SessionData {
+    currentUser?: number;
+  }
 }
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    // Configure CORS
-    const corsOptions: CorsOptions = {
-        origin: true, // Allow requests from any origin
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
-    };
-    app.enableCors(corsOptions);
+  // Configure CORS
+  const corsOptions: CorsOptions = {
+    origin: true, // Allow requests from any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
 
-    app.use(
-        session({
-            secret: 'my-secret',
-            resave: false,
-            saveUninitialized: false,
-        }),
-    );
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
-    const config = new DocumentBuilder()
-        .setTitle('My Cargonaut')
-        .setDescription('My Cargonaut API description')
-        .setVersion('1.0')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('My Cargonaut')
+    .setDescription('My Cargonaut API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json({ limit: '50mb' }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '50mb' }));
 
-    await app.listen(8000);
+  await app.listen(8000);
 }
 bootstrap();
--- myCargonaut-server/src/user/user.service.ts
+++ myCargonaut-server/src/user/user.service.ts
@@ -1,76 +1,75 @@
 import {
-		BadRequestException,
-		Injectable,
-		NotFoundException,
+  BadRequestException,
+  Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserDB } from '../database/UserDB';
 import * as bcrypt from 'bcryptjs';
-import { LoginDTO } from "../session/DTO/LoginDTO";
-import { empty } from "rxjs";
+import { LoginDTO } from '../session/DTO/LoginDTO';
 
 @Injectable()
 export class UserService {
-		constructor(
-				@InjectRepository(UserDB)
-				private userRepository: Repository<UserDB>,
-		) {}
+  constructor(
+    @InjectRepository(UserDB)
+    private userRepository: Repository<UserDB>,
+  ) {}
 
-		// gets a single user from the database by their ID
-		async getUserById(id: number): Promise<UserDB> {
-				const user = await this.userRepository.findOne({ where: { id } });
-				if (!user) {
-						throw new NotFoundException('User not found');
-				}
-				return user;
-		}
+  // gets a single user from the database by their ID
+  async getUserById(id: number): Promise<UserDB> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
 
-		async getLoggingUser(body: LoginDTO): Promise<UserDB | undefined> {
-        const user: UserDB | null = await this.userRepository.findOne({
-            where: { email: body.email },
-        });
-        return user;
-		}
+  async getLoggingUser(body: LoginDTO): Promise<UserDB | undefined> {
+    const user: UserDB | null = await this.userRepository.findOne({
+      where: { email: body.email },
+    });
+    return user;
+  }
 
-		async createUser(
-				firstName: string,
-				lastName: string,
-				email: string,
-				password: string,
-				birthday: Date,
-				phoneNumber?: string,
-				profilePic?: string,
-		): Promise<UserDB> {
-				// Check if email already exists
-				const checkUser = await this.userRepository.findOne({
-						where: { email },
-				});
-				if (checkUser) {
-						throw new BadRequestException(
-								'Es exisitert bereits ein Nutzer mit dieser E-Mail-Adresse.',
-						);
-				}
+  async createUser(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    birthday: Date,
+    phoneNumber?: string,
+    profilePic?: string,
+  ): Promise<UserDB> {
+    // Check if email already exists
+    const checkUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (checkUser) {
+      throw new BadRequestException(
+        'Es exisitert bereits ein Nutzer mit dieser E-Mail-Adresse.',
+      );
+    }
 
-				const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-				const newUser: UserDB = this.userRepository.create();
-				newUser.firstName = firstName;
-				newUser.lastName = lastName;
-				newUser.email = email;
-				newUser.password = hashedPassword;
-				newUser.birthday = birthday;
-				newUser.phoneNumber = phoneNumber;
-				newUser.profilePic = profilePic;
-				return this.userRepository.save(newUser);
-		}
+    const newUser: UserDB = this.userRepository.create();
+    newUser.firstName = firstName;
+    newUser.lastName = lastName;
+    newUser.email = email;
+    newUser.password = hashedPassword;
+    newUser.birthday = birthday;
+    newUser.phoneNumber = phoneNumber;
+    newUser.profilePic = profilePic;
+    return this.userRepository.save(newUser);
+  }
 
-		async updateUser(userData: UserDB): Promise<UserDB> {
-				return this.userRepository.save(userData);
-		}
+  async updateUser(userData: UserDB): Promise<UserDB> {
+    return this.userRepository.save(userData);
+  }
 
-		async updatePassword(userData: UserDB): Promise<UserDB> {
-				userData.password = await bcrypt.hash(userData.password, 10);
-				return this.userRepository.save(userData);
-		}
+  async updatePassword(userData: UserDB): Promise<UserDB> {
+    userData.password = await bcrypt.hash(userData.password, 10);
+    return this.userRepository.save(userData);
+  }
 }
