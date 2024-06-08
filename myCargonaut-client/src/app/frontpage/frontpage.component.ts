6d4417b153401bd82fcd4cd1b15f6dfcbd141983
Fabian Fimmen <fabian@ourhost.de>
2024-06-08 12:53:03 +0200

    Carousel gefixt und bootstrap js in angular.json eingefügt

--- myCargonaut-client/src/app/frontpage/frontpage.component.ts
+++ myCargonaut-client/src/app/frontpage/frontpage.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import bootstrap from "../../main.server";
+import {NgbSlide} from "@ng-bootstrap/ng-bootstrap";
+import {CarouselComponent} from "./carousel/carousel.component";
+
 
 @Component({
   selector: 'app-frontpage',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbSlide,
+    CarouselComponent
+  ],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.css'
 })
