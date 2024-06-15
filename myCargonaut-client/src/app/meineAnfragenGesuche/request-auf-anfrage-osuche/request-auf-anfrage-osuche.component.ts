a6eabe0c84186e144518efb26706a1e76982dbce
Fabian Fimmen <fabian@ourhost.de>
2024-06-15 14:38:09 +0200

    request page done

---
+++ myCargonaut-client/src/app/meineAnfragenGesuche/request-auf-anfrage-osuche/request-auf-anfrage-osuche.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-auf-anfrage-osuche',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-auf-anfrage-osuche.component.html',
+  styleUrl: './request-auf-anfrage-osuche.component.css'
+})
+export class RequestAufAnfrageOSucheComponent {
+
+}
