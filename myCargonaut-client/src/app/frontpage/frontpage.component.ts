21972d83beccb182810b4b92f52bb30cbe4e7686
Fabian Fimmen <fabian@ourhost.de>
2024-06-17 01:05:40 +0200

    Bilder sind in der Frontpage drinne
    todo:
    
    Trips Offer: Bewertungen
    Trips Request: Bewertungen
    
    FrontPage: Bilder fixen
    
    searchOffer: Bewertungen angeben
    searchRequest: Bewertungen angeben

--- myCargonaut-client/src/app/frontpage/frontpage.component.ts
+++ myCargonaut-client/src/app/frontpage/frontpage.component.ts
@@ -4,6 +4,8 @@ import {NgbSlide} from "@ng-bootstrap/ng-bootstrap";
 import {CarouselComponent} from "./carousel/carousel.component";
 import {offer} from "../search/offers";
 import {HttpClient} from "@angular/common/http";
+import {NgOptimizedImage} from "@angular/common";
+
 
 
 @Component({
@@ -11,7 +13,8 @@ import {HttpClient} from "@angular/common/http";
   standalone: true,
   imports: [
     NgbSlide,
-    CarouselComponent
+    CarouselComponent,
+    NgOptimizedImage
   ],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.css'
@@ -19,16 +22,35 @@ import {HttpClient} from "@angular/common/http";
 export class FrontpageComponent {
 
   allOffers: offer[] = [];
-  offerBool: boolean = true;
+  pathToImage: string = "empty.png";
+  pathToImageArray: string[] = [];
 
   constructor(private http: HttpClient) { }
 
+  getImageUrl(index: number): string {
+    // @ts-ignore
+    const protocol = window.location.protocol;
+    // @ts-ignore
+    const host = window.location.host.replace('4200', '8000');
+    return `${protocol}//${host}/${this.pathToImageArray[index]}`;
+  }
+
   ngOnInit(): void {
+    const prePath: string = "/vehicle/image/";
+
+
     this.http.get("http://localhost:8000/drive/all/offers", { withCredentials: true })
       .subscribe(
         (response: any) => {
 
           for (let i = 0; i < 3; i++) {
+
+              const imagePath: string = response[i].carPicture;
+              this.pathToImage = imagePath === "empty.png" ? "/empty.png" : prePath.concat(imagePath);
+              this.pathToImageArray.push(this.pathToImage);
+
+
+              console.log(this.pathToImageArray);
 
             if(response[i] !== undefined) {
               this.allOffers.push(response[i]);
@@ -43,4 +65,6 @@ export class FrontpageComponent {
   }
 
   protected readonly offer = offer;
+  protected readonly window = window;
 }
+
