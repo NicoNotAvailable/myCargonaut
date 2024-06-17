0f0709a9a5ad960189d3ddc263e82411e0a41cd6
Fabian Fimmen <fabian@ourhost.de>
2024-06-17 12:32:28 +0200

    Componente erstellt und search_read gemerged todo:
    Trips Offer: Bewertungen
    Trips Request: Bewertungen
    
    searchOffer: Bewertungen angeben
    searchRequest: Bewertungen angeben

---
+++ myCargonaut-client/src/app/trips-create/trips-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trips-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './trips-create.component.html',
+  styleUrl: './trips-create.component.css'
+})
+export class TripsCreateComponent {
+
+}
