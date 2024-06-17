2f839ff0977816749610b79bfb78dd36e34cb36f
Fabian Fimmen <fabian@ourhost.de>
2024-06-17 14:33:44 +0200

    HTML für Create Request und Offer Trips -> Cargo muss noch erstellt werden
    todo:
    
    Trips Offer: Bilder und Bewertungen
    Trips Request: Siehe Trips Offer
    
    FrontPage: Bilder fixen
    searchOffer: Bewertungen angeben und Bilder fixen
    searchRequest: siehe searchOffer

--- myCargonaut-client/src/app/trips-create/trips-create.component.ts
+++ myCargonaut-client/src/app/trips-create/trips-create.component.ts
@@ -18,5 +18,8 @@ export class TripsCreateComponent {
   bewertungsDummy : number = 4;
   stars: number[] = [1, 2, 3, 4, 5];
 
+  offerBool: boolean = false;
+  requestBool: boolean = true;
+
   protected readonly window = window;
 }
