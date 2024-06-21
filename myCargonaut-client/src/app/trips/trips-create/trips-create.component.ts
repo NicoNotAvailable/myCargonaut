e1cb32d1787f6d9fd077bf31bd49fc492cc80800
tinahsr <tinahaser@gmail.com>
2024-06-21 19:44:38 +0200

    correct stuff displayed

--- myCargonaut-client/src/app/trips/trips-create/trips-create.component.ts
+++ myCargonaut-client/src/app/trips/trips-create/trips-create.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {NgForOf, NgOptimizedImage} from "@angular/common";
+import { ActivatedRoute } from '@angular/router';
+import { DriveService } from '../../services/drive.service';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-trips-create',
@@ -11,7 +14,23 @@ import {NgForOf, NgOptimizedImage} from "@angular/common";
   templateUrl: './trips-create.component.html',
   styleUrl: './trips-create.component.css'
 })
-export class TripsCreateComponent {
+export class TripsCreateComponent implements OnInit {
   offerBool: boolean = true;
   requestBool: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if (params ['type'] == "offer") {
+        this.offerBool = true;
+      } else if (params ['type'] == "request") {
+        this.requestBool = true
+        this.offerBool = false;
+      }
+    });
+  }
 }
