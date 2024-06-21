1bd99441ea7960c43f53d9c5d4f0064e9e576b7b
Stefan Ganswint <stefan.ganswint@mni.thm.de>
2024-06-21 16:13:02 +0200

    Review read mit Profil Pic rdy

--- myCargonaut-client/src/app/review/ReviewReadComponent/review-read.component.ts
+++ myCargonaut-client/src/app/review/ReviewReadComponent/review-read.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { NgClass, NgForOf } from '@angular/common';
+import { NgClass, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
 import { faSave, faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 import { HttpClient } from '@angular/common/http';
+import { DateUtils } from '../../../../../utils/DateUtils';
+import { SessionService } from '../../services/session.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-ReviewRead' ,
@@ -12,15 +15,25 @@ import { HttpClient } from '@angular/common/http';
     FaIconComponent,
     NgForOf,
     NgClass,
+    NgIf,
+    NgOptimizedImage,
   ],
   templateUrl: './review-read.component.html',
   styleUrl: './review-read.component.css'
 })
 export class ReviewReadComponent {
 
+  public sessionService: SessionService = inject(SessionService);
+  public userService: UserService = inject(UserService);
+
   protected readonly faStar = faStar;
   protected readonly faSave = faSave;
   faStarRegular = faStarRegular;  // Empty star icon
+
+
+  pathToImage: string = "empty.png";
+  profilePic: string = "";
+
 
 
   reviewTitle: string = "";
@@ -37,6 +50,7 @@ export class ReviewReadComponent {
   }
 
   ngOnInit(): void {
+    this.readUser()
     this.getReview()
 
   }
@@ -80,5 +94,27 @@ export class ReviewReadComponent {
     return Array.from({ length: 5 }, (_, i) => i < rating);
   }
 
+  readUser(): void {
 
+    const prePath: string = "assets/";
+
+    setTimeout(() => {
+      this.pathToImage = "";
+      const imagePath: string = "empty.png";
+      this.pathToImage = prePath.concat(imagePath);
+      this.sessionService.checkLoginNum();
+    }, 200);
+    this.userService.readUser().subscribe(
+      response => {
+        const imagePath: string = response.profilePic;
+        this.profilePic = response.profilePic;
+        this.pathToImage = imagePath === "empty.png" ? "assets/empty.png" : prePath.concat(imagePath);
+      },
+      error => {
+        console.error("There was an error!", error);
+      }
+    );
+  }
+
+  protected readonly window = window;
 }
