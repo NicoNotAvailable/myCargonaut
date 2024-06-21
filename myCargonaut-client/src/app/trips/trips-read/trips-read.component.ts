7684f8055615efca3b1b8759826f635829a2fc60
Alexander Mutig <alexander.mutig@mni.thm.de>
2024-06-21 21:01:32 +0200

    Added trip read html & css

--- myCargonaut-client/src/app/trips/trips-read/trips-read.component.ts
+++ myCargonaut-client/src/app/trips/trips-read/trips-read.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-trips-read',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './trips-read.component.html',
   styleUrl: './trips-read.component.css'
 })
