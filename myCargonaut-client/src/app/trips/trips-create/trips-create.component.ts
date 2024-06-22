53bc48621cd3f3c40e4f5848c916f48aa40b42d7
tinahsr <tinahaser@gmail.com>
2024-06-22 19:53:38 +0200

    cargo stuff added

--- myCargonaut-client/src/app/trips/trips-create/trips-create.component.ts
+++ myCargonaut-client/src/app/trips/trips-create/trips-create.component.ts
@@ -1,26 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit, TemplateRef } from '@angular/core';
 import {NgForOf, NgOptimizedImage} from "@angular/common";
 import { ActivatedRoute } from '@angular/router';
 import { offer } from '../../search/offers';
 import { GetOffer } from '../GetOffer';
 import { GetRequest } from '../GetRequest';
+import { faCircle, faCirclePlus, faPenToSquare, faSave, faX } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Cargo } from '../../drive/Cargo';
+import { RequestService } from '../../../drive/request.service';
+import { SessionService } from '../../services/session.service';
+
 
 @Component({
   selector: 'app-trips-create',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NgForOf
+    NgForOf,
+    FaIconComponent,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './trips-create.component.html',
   styleUrl: './trips-create.component.css'
 })
 export class TripsCreateComponent implements OnInit {
+  public sessionService: SessionService = inject(SessionService);
+  public requestService: RequestService = inject(RequestService);
+
   offerBool: boolean = true;
   requestBool: boolean = false;
+  private cargoModalRef: NgbModalRef | undefined;
+  //Cargo
+  cargoDescription: string | null = null;
+  cargoLength: number | null = null;
+  cargoWidth: number | null = null;
+  cargoHeight: number | null = null;
+  cargoWeight: number | null = null;
 
   constructor(
     private route: ActivatedRoute,
+    private modalService: NgbModal,
 
   ) {}
 
@@ -36,6 +58,41 @@ export class TripsCreateComponent implements OnInit {
     });
   }
 
+  openCargoModal(content: TemplateRef<any>) {
+    this.cargoModalRef = this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  addCargoToArray(modal: any) {
+    if (this.cargoDescription != null && this.cargoWeight != null &&
+      this.cargoLength != null && this.cargoWidth != null &&
+      this.cargoHeight != null) {
+      const newCargo = new Cargo(this.cargoDescription, this.cargoWeight, this.cargoLength, this.cargoWidth, this.cargoHeight);
+      this.requestService.addCargo(newCargo);
+      console.log('Cargo added:', newCargo);
+
+      this.cargoDescription = null;
+      this.cargoWeight = null;
+      this.cargoLength = null;
+      this.cargoWidth = null;
+      this.cargoHeight = null;
+    } else {
+
+    }
+    // Close the modal
+    if (this.cargoModalRef) {
+      this.cargoModalRef.close();
+    }
+  }
+
+  get cargoDataArray() {
+    return this.requestService.getCargos();
+  }
+
   @Input() offer!: GetOffer | undefined;
   @Input() request!: GetRequest | undefined;
+  protected readonly faSave = faSave;
+  protected readonly faCircle = faCircle;
+  protected readonly faCirclePlus = faCirclePlus;
+  protected readonly faX = faX;
+  protected readonly faPenToSquare = faPenToSquare;
 }
